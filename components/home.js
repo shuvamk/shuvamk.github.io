fae76f50919b950e8d1248b30ddd968a491f0622
shuvamk <kshuvam564@gmail.com>
2021-04-15 02:50:23 +0530

    Created Next app

--- components/home.js
+++ components/home.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 function Home() {
-  return <div></div>;
+  return (
+    <div>Hello There, I am Shuvam Kumar. I love experimenting with the web</div>
+  );
 }
 
 export default Home;
