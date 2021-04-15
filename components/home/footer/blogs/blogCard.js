45bf5ead96c812233deae5ec183885b4a54c622c
shuvamk <kshuvam564@gmail.com>
2021-04-15 20:57:44 +0530

    Card changes

--- components/home/footer/blogs/blogCard.js
+++ components/home/footer/blogs/blogCard.js
@@ -2,7 +2,21 @@ import React from "react";
 import styles from "./blogCard.module.css";
 
 function BlogCard() {
-  return <div className={styles.blogCard}></div>;
+  return (
+    <div className={styles.blogCard}>
+      <div className={styles.title}>This is Title</div>
+      <div className={styles.description}>
+        This is the body of the blog, consider it to be very big so that it can
+        be ellipsed and I can create a good design for all muy blogs. This has
+        nothing to do with apple and mango.
+      </div>
+      <div className={styles.taglist}>
+        <div className={styles.tag}>Apple</div>
+        <div className={styles.tag}>Apple</div>
+        <div className={styles.tag}>Apple</div>
+      </div>
+    </div>
+  );
 }
 
 export default BlogCard;
