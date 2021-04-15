112b0f051d30a90eaf39dd3326258915ed5d3e4f
shuvamk <kshuvam564@gmail.com>
2021-04-15 18:50:43 +0530

    Blog button done

--- components/home/footer/blogs/blogs.js
+++ components/home/footer/blogs/blogs.js
@@ -1,6 +1,7 @@
 import React from "react";
 import BlogCard from "./blogCard";
 import styles from "./blogs.module.css";
+import Image from "next/image";
 
 function Blogs() {
   return (
@@ -16,7 +17,15 @@ function Blogs() {
 
       <div className={styles.footer}>
         <button className={styles.allButton}>
-          <span className={styles.buttonTitle}>See all</span>
+          <span className={styles.buttonTitle}>
+            See all
+            <Image
+              src="/assets/next.png"
+              height="24"
+              width="24"
+              className={styles.nextIcon}
+            />
+          </span>
         </button>
       </div>
     </div>
